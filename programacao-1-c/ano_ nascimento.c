0fdc3c499531601e16af345ae736ff30c04f1bd3
Paulo <pcfdaa@gmail.com>
2024-09-22 16:40:34 -0300

    update files

--- programacao-1-c/ano_ nascimento.c
+++ programacao-1-c/ano_ nascimento.c
@@ -1,1 +1,15 @@
-// WIP
+#include <stdio.h>
+
+int main() {
+  int idade, ano_nascimento, ano_atual;
+  ano_atual = 2024;
+
+  printf("Informe a sua idade: \n");
+  scanf("%d", &idade);
+
+  ano_nascimento = ano_atual - idade;
+  printf("Você nasceu em: %d", ano_nascimento);
+
+  // Terminar a função
+  return 0;
+}
